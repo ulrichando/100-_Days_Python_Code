8b0add183e99239d65e49765ea29cf98c0cae5ea
Ulrich Ando <ulrichando007@gmail.com>
2023-08-07 22:31:45 -0400

    deleted input_function

---
+++ Day_1/input_exercise.py
@@ -0,0 +1,8 @@
+# This code input the
+
+name = input("What is your name ?")
+print(len(name))
+
+# shorter version of the code
+
+print(len(input("what is your name ?")))
--- Day_1/input_function.py
+++ Day_1/input_function.py
@@ -1,2 +1,1 @@
-input("what is your name ")
-input(" A prompt for the user ")
+print("Hello" + input("What is your name?") + "!")
