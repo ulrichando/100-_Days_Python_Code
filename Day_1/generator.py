345979fd8cd6a4f31b507aa14c1e4a7756725885
Ulrich Ando <ulrichando007@gmail.com>
2023-08-12 18:34:18 -0400

    generator

---
+++ Day_1/generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the brand name genrator")
+
+cityName = input("What city did you grew up in : \n")
+
+petName = input("What is your pet's name : \n")
+
+print("Your band name is " + cityName + " " + petName)
